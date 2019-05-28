1e2a0a911d3b9a76e95459958ee2dc40e67f70a3
CaioTorre <caiosanches@live.com>
2019-05-27 21:41:26 -0300

    Returning to python files

--- ISee.py
+++ ISee.py
@@ -24,6 +24,7 @@ class Ensemble:
             
         self.encoder = None
         self.tokenizer = None
+        self.rev_token_map = None
         self.n_folds = n_folds
         self.skfold = KFold(n_splits = n_folds, shuffle=True, random_state=1)
         #self.raw_avg = None
@@ -52,7 +53,7 @@ class Ensemble:
         self.datasets['original']['y'] = y
         self.datasets['original']['i'] = indexes
         
-    def split_data(self, shuffle_seed=None, purge_duplicates=True):
+    def split_data(self, purge_duplicates=True):
         if self.datasets['original']['y'] == []:
             raise RuntimeError("No dataset configured - use Ensemble.set_datasets(x, y)")
         if self.tokenizer == None:
@@ -82,8 +83,6 @@ class Ensemble:
             _y = self.encoder.transform(self.datasets['original']['y']).tolist()[:]
             _i = self.datasets['original']['i'][:]
         
-        #skfind = [index for index in self.skfold]
-        
         n = 0
         for train_indexes, test_indexes in self.skfold.split(_x, _y, _i):
             self.datasets['split'][n]['train']['x'] = [_x[v] for v in train_indexes]
@@ -95,7 +94,11 @@ class Ensemble:
             n += 1
         
     def get_string_from_tokenizer(self, vec):
-        return dict(map(vec, self.tokenizer.word_index.items()))
+        #print("Vec = " + str(vec))
+        #print("WI  = " + str(self.tokenizer.word_index))
+        #print("I() = " + str(self.tokenizer.word_index.items()))
+        #return dict(map(vec, self.tokenizer.word_index.items()))
+        return [self.rev_token_map.get(letter) for letter in vec]
         
     def test_sets(self, ignore_collisions=True):
         print("Testing datasets for discrepancies...")
@@ -145,6 +148,7 @@ class Ensemble:
         if self.tokenizer == None:
             raise RuntimeError("Tokenizer is not configured - use Ensemble.set_tokenizer(new_tokenizer)")
         self.tokenizer.fit_on_texts(self.datasets['original']['x'])
+        self.rev_token_map = dict(map(reversed, self.tokenizer.word_index.items()))
         
     def train(self, max_features, maxlen, profiler=None):
         if self.datasets['original']['x'] == []:
@@ -181,10 +185,10 @@ class Ensemble:
             _y_categorical_val = np_utils.to_categorical(self.datasets['split'][n]['test']['y'])
             
             for _m_key, _m_model in self.models[n].items():
-                if profiler is not None:
-                    profiler.add_milestone("Fold {} - Training model {}".format(n + 1, _m_key))
-                else:
-                    print("\tTraining model " + _m_key)
+                #if profiler is not None:
+                #    profiler.add_milestone("Fold {} - Training model {}".format(n + 1, _m_key))
+                #else:
+                #    print("\tTraining model " + _m_key)
                     
                 if _m_model['model_one_hot']:
                     _x = _x_one_hot
@@ -211,8 +215,8 @@ class Ensemble:
                 #except Exception:
                 #    _m_model['model'].fit(_x, _y)
                 
-                #if profiler is not None:
-                #    profiler.add_milestone("Fold {} - Trained model {}".format(n + 1, _m_key))
+                if profiler is not None:
+                    profiler.add_milestone("Fold {} - Trained model {}".format(n + 1, _m_key))
         
             #for _m_key, _m_model in self.models[n].items():
             #    if _m_model['histories'] is not None:
@@ -271,10 +275,10 @@ class Ensemble:
             _x_orig = sequence.pad_sequences(np.array(self.datasets['split'][n]['test']['x']), maxlen=maxlen)
             
             for _m_key, _m_model in self.models[n].items():
-                if profiler is not None:
-                    profiler.add_milestone("Fold {} - Testing model {}".format(n + 1, _m_key))
-                else:
-                    print("\tTesting model " + _m_key)
+                #if profiler is not None:
+                #    profiler.add_milestone("Fold {} - Testing model {}".format(n + 1, _m_key))
+                #else:
+                #    print("\tTesting model " + _m_key)
                     
                 if _m_model['model_one_hot']:
                     _x = _x_one_hot
@@ -293,8 +297,9 @@ class Ensemble:
         print("Finished testing")
         return _predicts
         
-    def evaluate(self, verbose=False, print_individual_results=True, file_d=None):
+    def evaluate(self, verbose=False, print_individual_results=True, file_d=None, csv_file=None):
         _avgs = {'Ensemble':0.0}
+        _comb_total = 0
         for n in range(self.n_folds):
             print("Evaluating {}-th fold".format(n + 1))
             if file_d is not None: file_d.write("Evaluating {}-th fold\n".format(n + 1))
@@ -302,6 +307,12 @@ class Ensemble:
             for key in self.models[n]:
                 _correct_guesses[key] = 0
                 
+            csv_line = "{}\t{}\t{}".format("Utterance", "Reversed tokens", "Correct classification")
+            for key in self.models[n]:
+                csv_line += "\t{} classification".format(key)
+            csv_line += "\t{}\n".format("Majority vote")
+            if csv_file is not None: csv_file.write(csv_line)
+            
             if verbose:
                 header_msg = "\t     -  Correct "
                 for key in self.models[n]:
@@ -310,10 +321,13 @@ class Ensemble:
                 print(header_msg + "- Phrase")
                 
             total = len(self.datasets['split'][n]['test']['y'])
+            _comb_total += total
             for this_n in range(total - 1):
                 _correct_predict = self.datasets['split'][n]['test']['y'][this_n]
                 _correct_predict_strn = self.encoder.inverse_transform([_correct_predict])[0]
                 msg = "\t%4d - %10s " % (this_n, _correct_predict_strn)
+                #In BETA !
+                csv_line = "{}\t{}\t{}".format(self.datasets['original']['x'][self.links[n]['test'][this_n]], ' '.join(self.get_string_from_tokenizer(self.datasets['split'][n]['test']['x'][this_n])), _correct_predict_strn)
                 votes = []
                 
                 for _m_key, _m_model in self.models[n].items():
@@ -325,6 +339,7 @@ class Ensemble:
                         msg += "- %10s* " % (self.encoder.inverse_transform([model_vote])[0])
                     else:
                         msg += "- %10s  " % (self.encoder.inverse_transform([model_vote])[0])
+                    csv_line += "\t{}".format(self.encoder.inverse_transform([model_vote])[0])
                         
                         
                 major_vote = max(votes, key=votes.count)
@@ -333,31 +348,33 @@ class Ensemble:
                     msg += "- %10s* " % (self.encoder.inverse_transform([major_vote])[0])
                 else:
                     msg += "- %10s  " % (self.encoder.inverse_transform([major_vote])[0])
+                csv_line += "\t{}\n".format(self.encoder.inverse_transform([major_vote])[0])
                 
+                if csv_file is not None: csv_file.write(csv_line)
                 if verbose:
                     print(msg + self.datasets['original']['x'][self.links[n]['test'][this_n]])
-                    if file_d is not None: file_d.write(msg + self.datasets['original']['x'][self.links[n]['test'][this_n]] + '\n')
+                if file_d is not None: file_d.write(msg + self.datasets['original']['x'][self.links[n]['test'][this_n]] + '\n')
                     
             if print_individual_results:
                 for name in self.models[n]:
                     print("\t" + name + " got {} out of {} ({}%)".format(_correct_guesses[name], total, 100. * _correct_guesses[name] / total))
                     if file_d is not None: file_d.write("\t" + name + " got {} out of {} ({}%)\n".format(_correct_guesses[name], total, 100. * _correct_guesses[name] / total))
                     try:
-                        _avgs[name] += _correct_guesses[name] / total
+                        _avgs[name] += _correct_guesses[name]
                     except KeyError:
-                        _avgs[name] = _correct_guesses[name] / total
+                        _avgs[name] = _correct_guesses[name]
                 print("\tEnsemble got {} out of {} ({}%)".format(_correct_guesses['Major'], total, 100. * _correct_guesses['Major'] / total))
                 if file_d is not None: file_d.write("\tEnsemble got {} out of {} ({}%)\n".format(_correct_guesses['Major'], total, 100. * _correct_guesses['Major'] / total))
-                _avgs['Ensemble'] += _correct_guesses['Major'] / total
+                _avgs['Ensemble'] += _correct_guesses['Major'] 
         
         if print_individual_results:
             print("Across {} folds, averages are:".format(self.n_folds))
             if file_d is not None: file_d.write("Across {} folds, averages are:\n".format(self.n_folds))
             for name in self.models[0]:
-                print("\t" + name + ": %.3f%%" % (100. * _avgs[name] / self.n_folds))
-                if file_d is not None: file_d.write("\t" + name + ": %.3f%%\n" % (100. * _avgs[name] / self.n_folds))
-            print("\tEnsemble: %.3f%%" % (100. * _avgs['Ensemble'] / self.n_folds))
-            if file_d is not None: file_d.write("\tEnsemble: %.3f%%\n" % (100. * _avgs['Ensemble'] / self.n_folds))
+                print("\t" + name + ": %.3f%%" % (100. * _avgs[name] / _comb_total))
+                if file_d is not None: file_d.write("\t" + name + ": %.3f%%\n" % (100. * _avgs[name] / _comb_total))
+            print("\tEnsemble: %.3f%%" % (100. * _avgs['Ensemble'] / _comb_total))
+            if file_d is not None: file_d.write("\tEnsemble: %.3f%%\n" % (100. * _avgs['Ensemble'] / _comb_total))
         return total, _correct_guesses
         
     def to_ordinal(self, num):
--- argy.py
+++ argy.py
@@ -14,12 +14,12 @@ class Args:
             else:
                 self.nested_params[last_p].append(_p)
         
-    def add(self, param_alias, param_type=str default_value=None):
+    def add(self, param_alias, param_type=str, default_value=None):
         if param_alias in self.args:
             raise ValueError('Parameter {} already found in list'.format(param_alias))
         self.args[param_alias] = {'p_type': param_type, 'd_value': default_value}
         
-    def get(self, param_alias):
+    def get(self, param_alias, cut_one=True):
         if param_alias not in self.args:
             raise ValueError('Parameter {} not found in watchlist'.format(param_alias))
         if param_alias not in self.nested_params:
@@ -27,6 +27,8 @@ class Args:
         ret_list = []
         for _v in self.nested_params[param_alias]:
             ret_list.append(self.args[param_alias]['p_type'](_v))
+        if cut_one and len(ret_list) == 1:
+            return ret_list[0]
         return ret_list
         
     def getAllInputsAsList(self):
